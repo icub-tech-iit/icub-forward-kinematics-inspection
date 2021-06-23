ad38377124fa1731a751cf97ea98a80a0643f0e3
Mattia Fussi <mattia.fussi@iit.it>
2021-06-23 17:35:40 +0200

    Added new poses and created plots to analyze misalignment between frames

--- compare_tables.m
+++ compare_tables.m
@@ -42,10 +42,21 @@ RobotIDyn.tool = h;
 
 qdes1 = [0 0 0 0 0 0 60.5 0 0 0];
 qdes2 = [0 0 0 0 0 0 90.5 0 0 0];
-qdes3 = [0 0 0 0 90 -30 15 0 0 0];
+qdes3 = [0 0 0 0 135 0 90.5 0 0 0];
 qdes4 = [0 0 0 0 135 0 90.5 -90 -30.6 20.4];
+qdes5 = [25 0 -9.6, 9.45 160.80 79.56 19.005 0 0 0.6];
+qdes6 = [25 10 -9.6, 9.45 160.80 79.56 19.005 0 0 0.6];
+qdes7 = [25 -15 -9.6, 9.45 160.80 79.56 19.005 0 0 0.6];
+qdes8 = [-25 30 -9.6, 9.45 160.80 79.56 19.005 0 0 0.6];
 
 Q = deg2rad(qdes2);
+RobotIDyn.display
+RobotIDyn.fkine(Q)
+
+RobotSource.display
+RobotSource.fkine(Q)
+
+%% Plot Arms
 
 hold on
 RobotSource.plot(Q, 'jointcolor', 'b', 'linkcolor', 'r', 'jointdiam', 0.5, ...
@@ -53,24 +64,91 @@ RobotSource.plot(Q, 'jointcolor', 'b', 'linkcolor', 'r', 'jointdiam', 0.5, ...
     'noshading', 'noname', 'noshadow');
     zlim([-1, 1]);
 
-alpha(.5)
+alpha(.05)
 hold on
 RobotIDyn.plot(Q, 'jointcolor', 'r', 'linkcolor', 'b', 'jointdiam', 0.5,...
     'nojoints', 'workspace', [-1 1 -1 1 -1 1], ...
     'noshading',  'noname', 'noshadow', 'nobase');
     zlim([-0.5, 0.6]);
-alpha(.5)
+alpha(.05)
 
 annotation('textbox', [.5 .5 .3 .3], 'String', 'red: iKin   blue: iDynTree','FitBoxToText','on');
-exportgraphics(gcf(), 'zeros.png');
 
-RobotIDyn.display
-RobotIDyn.fkine(Q)
 
-RobotSource.display
-RobotSource.fkine(Q)
+%% Test links transforms
+ntrials = 4;
+Q = zeros(ntrials, 10);
+Q(1, :) = deg2rad(qdes5);
+Q(2, :) = deg2rad(qdes6);
+Q(3, :) = deg2rad(qdes7);
+Q(4, :) = deg2rad(qdes8);
 
-errormatrix = sqrt((RobotIDyn.fkine(Q) - RobotSource.fkine(Q)).^2)
+e_qdes1 = zeros(10, 3, ntrials);
 
-model = importrobot('model/model.urdf');
-fromtorso = subtree(model, 'root_link');
+for k=1:ntrials
+[iKinHee, iKinTransforms] = RobotSource.fkine(Q(k, :));
+[iDynTreeHee, iDynTreeTransforms] = RobotIDyn.fkine(Q(k, :));
+    for i=1:10
+        e_qdes1(i, :, k) = iDynTreeTransforms(i).transl - iKinTransforms(i).transl;
+    end
+end 
+figure('renderer', 'painters')
+subplot(3,1,1)
+hold on
+grid minor
+for i=1:ntrials
+    e = e_qdes1(:, 1, i);
+    plot(e * 1000, 'o-', 'LineWidth', 1.2);
+end
+legend({'test5', 'test6', 'test7', 'test8', 'test5'});
+title('Translational error between iDynTree and iKin with four poses');
+ylabel('error X [mm]');
+
+subplot(3,1,2)
+hold on
+grid minor
+for i=1:ntrials
+    e = e_qdes1(:, 2, i);
+plot(e * 1000, 'o-', 'LineWidth', 1.2);
+end
+ylabel('error Y [mm]');
+
+subplot(3,1,3)
+hold on
+grid minor
+for i=1:ntrials
+    e = e_qdes1(:, 3, i);
+    plot(e * 1000, 'o-', 'LineWidth', 1.2);
+end
+ylabel('error Z [mm]');
+xlabel('reference frame index');
+%% Test  500 random configurations
+
+njoints = 10;
+ntrials = 500;
+std = 60;
+qrand = fix(std.*rand(ntrials, njoints));
+
+IdynTransl = zeros(ntrials, 3);
+SourceTransl = zeros(ntrials, 3);
+for i=1:ntrials
+    H = RobotIDyn.fkine(qrand(i, :)).double;
+    IdynTransl(i, :) = H(1:3, 4);
+    H = RobotSource.fkine(qrand(i, :)).double;
+    SourceTransl(i, :) = H(1:3, 4) ;
+end
+
+e_trials = IdynTransl - SourceTransl;
+subplot(3,1,1)
+histogram(e_trials(:,1) * 1000, 50)
+subplot(3,1,2)
+histogram(e_trials(:,2) * 1000, 50)
+subplot(3,1,3)
+histogram(e_trials(:,3) * 1000, 50)
+grid minor
+legend({'e_x','e_y','e_z'})
+ylabel('n. occurrences')
+xlabel('Error [mm]')
+%% Test handpicked configs
+
+anglesdeg = repmat([-90, -60, -30, -15, 0, 15, 30, 60, 90]', 1, njoints);
